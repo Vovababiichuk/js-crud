90910aab7c0feea84b0b2c9283c8bac388d06c86
Vovababiichuk <babihuk888@gmail.com>
2023-08-10 13:56:57 +0300

    added audio + adaptive

--- public/main.js
+++ public/main.js
@@ -40,3 +40,16 @@ updateForm.addEventListener('submit', function (event) {
     )
   }
 })
+
+const audio = document.getElementById("myAudio");
+const playButton = document.getElementById("playButton");
+
+function togglePlay() {
+  if (audio.paused) {
+    audio.play();
+    playButton.innerHTML = '<i class="fa fa-pause pulsating-icon"></i>';
+  } else {
+    audio.pause();
+    playButton.innerHTML = '<i class="fa fa-play"></i>';
+  }
+}
