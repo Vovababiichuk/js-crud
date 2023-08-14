5b29bc87a45fb9f80f2a1f21dfbf0e1b336b61d1
Vovababiichuk <babihuk888@gmail.com>
2023-08-15 01:12:31 +0300

    added js-practice-crud-2-product

--- public/main.js
+++ public/main.js
@@ -1,45 +1,7 @@
-const createForm = document.querySelector('#myForm form')
-const updateForm = document.querySelector(
-  'form[action="/user-update"]',
-)
-
-createForm.addEventListener('submit', function (event) {
-  const inputs = createForm.querySelectorAll('input')
-  let isValid = false
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.trim() !== '') {
-      isValid = true
-      break
-    }
-  }
-
-  if (!isValid) {
-    event.preventDefault()
-    alert(
-      'Будь ласка, заповніть хоча б одне поле перед відправкою форми.',
-    )
-  }
-})
-
-updateForm.addEventListener('submit', function (event) {
-  const inputs = updateForm.querySelectorAll('input')
-  let isValid = false
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value.trim() !== '') {
-      isValid = true
-      break
-    }
-  }
-
-  if (!isValid) {
-    event.preventDefault()
-    alert(
-      'Будь ласка, заповніть хоча б одне поле перед відправкою форми.',
-    )
-  }
-})
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+});
 
 // Audio===========================
 
@@ -56,5 +18,6 @@ function togglePlay() {
   }
 }
 
-// Heading Page===================
+// ===================
+
 
