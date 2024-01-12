72d10fb1a1ead791f7809d6a080a68c30aeba821
Vovababiichuk <babihuk888@gmail.com>
2024-01-13 00:42:57 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  entry: './public/index.js',
+  entry: './index.js',
   output: {
     path: path.join(__dirname, '/public'),
     publicPath: '/public/dist',
@@ -23,4 +24,27 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util"),
+      "path": require.resolve("path-browserify"),
+      "url": require.resolve("url"),
+      "buffer": require.resolve("buffer/"),
+      fs: false,
+      constants: require.resolve("constants-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      assert: require.resolve("assert/"),
+      querystring: require.resolve("querystring-es3"),
+      crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    os: require.resolve("os-browserify/browser"),
+    net: require.resolve("node-libs-browser/mock/net"),
+    }
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    // ...
+  ],
 }
